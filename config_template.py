c198b0ec58e1f5000235531407789a8c48fe3a8a
SuperPaulTh <paul.thuencher@gmail.com>
2018-05-30 21:40:39 +0000

    Added config_template.py

---
+++ config_template.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Standard modules """
+import logging
+
+""" Config Template
+
+	PLEASE NOTE: You must rename this file to 'config.py' otherwise it will not work
+
+"""
+
+""" Bot Token """
+BOT_TOKEN = ''
+
+""" General Settings """
+AUTO_START_WAVES_BOT = True
+
+""" Development """
+LOG_LEVEL = logging.DEBUG
+
+""" Contact Information """
+COMPANY_NAME = 'COMPANY_NAME'
+COMPANY_EMAIL = 'MAIL@COMPANY.COM'
