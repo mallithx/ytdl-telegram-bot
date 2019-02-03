fa3c1db85eb7b90dcc55d23be429edcb7bc7f377
SuperPaulTh <paul.thuencher@gmail.com>
2019-02-03 15:24:38 +0100

    Added proper status display

--- src/commands/share.py
+++ src/commands/share.py
@@ -19,31 +19,33 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 log = logging.getLogger(__name__)
 log.setLevel(config.LOG_LEVEL)
 
-EXT = 'mp3'
-
 def handle_shared_url(bot, update, chat_data):
     url = update.message.text
-    log.info('Start handling url: %s' % url)
+    chat_id = update.message.chat.id
+    log.info('info "%s"' % url)
 
-
+    # display prepare message
+    msg_prepare = bot.send_message(chat_id=chat_id, text='<i>... please wait ...</i>', parse_mode=ParseMode.HTML)
 
     try:
-        process = subprocess.check_output(['youtube-dl', url, '--print-json', '-f bestaudio', '-x', '--audio-format', EXT, '--id'])
+        process = subprocess.check_output(['youtube-dl', '-s', url, '--print-json', '--id'])
         data = json.loads(process.decode('utf8'))
 
+        # remove prepare message
+        bot.delete_message(chat_id=chat_id, message_id=msg_prepare.message_id)
 
         _id = data['id']
         title = data['title']
         uploader = data['uploader']
-        ext = data['ext']
-        filename = '%s.%s' % (_id, EXT) # button data is limited to 64 bytes
 
-        chat_data.update({'filename': filename})
+        # set chat data
+        chat_data.update({'url': url})
 
-        log.debug('chat_data 0:\n{}'.format(chat_data))
+        # reply keyboard
+        keyboard = InlineKeyboardMarkup([
+                [InlineKeyboardButton('Download .mp3', callback_data='mp3')],
+                [InlineKeyboardButton('Download .wav', callback_data='wav')]])
 
-
-        keyboard = InlineKeyboardMarkup([[InlineKeyboardButton('Download Audio', callback_data=filename)]])
         update.message.reply_text(
             text='<strong>%s</strong>\n<i>uploaded by %s</i>' % (title, uploader), 
             parse_mode=ParseMode.HTML,
@@ -59,22 +61,32 @@ def handle_shared_url(bot, update, chat_data):
 def handle_provide_download(bot, update, chat_data):
     query = update.callback_query
     chat_id = query.message.chat.id
-    filename = query.data
 
-    log.debug('chat_data 1:\n{}'.format(chat_data))
+    ext = query.data
+    url = chat_data['url']
+    log.info('download "%s"' % url)
 
     # remove download btn after click
     bot.edit_message_reply_markup(query.message.chat.id, query.message.message_id)
 
     # display prepare message
-    msg_prepare = bot.send_message(chat_id=chat_id, text='<i>preparing file download ...</i>', parse_mode=ParseMode.HTML)
+    msg_prepare = bot.send_message(chat_id=chat_id, text='<i>... please wait ...</i>', parse_mode=ParseMode.HTML)
 
+    # download file
+    process = subprocess.check_output(['youtube-dl', url, '--print-json', '-f bestaudio', '-x', '--audio-format', ext, '--id'])
+    data = json.loads(process.decode('utf8'))
+
+    _id = data['id']
+    filename = '%s.%s' % (_id, ext) # button data is limited to 64 bytes
+
+    # send file
     bot.send_audio(chat_id=chat_id, audio=open(filename, 'rb'), timeout=1000)
     log.info('send "%s" done' % filename)
 
     # remove prepare message
     bot.delete_message(chat_id=chat_id, message_id=msg_prepare.message_id)
 
+    # remove file
     process = subprocess.check_output(['rm', '-f', filename])
     log.info('delete "%s" done' % filename)
 
