ad3ed01c3215a18f5b4162f5a6c9b26eca44a331
SuperPaulTh <paul.thuencher@gmail.com>
2019-04-19 20:08:04 +0200

    Implement inline button menues

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import logging
 import signal
 import sys
 import json
+import datetime
 import os
 from io import BytesIO
 
@@ -56,43 +57,80 @@ def handle_incoming_url(bot, update, chat_data):
                 '<strong>Error:</strong> <i>Given url is invalid or from an unsupported source.</i>', parse_mode=ParseMode.HTML)
             return ConversationHandler.END
 
-    # TODO: create format keyboard
+    meta = chat_data['metadata'] = {
+        'title': info_dict['title'] if 'title' in info_dict else '',
+        'performer': info_dict['creater'] if 'creater' in info_dict else info_dict['uploader'],
+        'duration': str(datetime.timedelta(seconds=int(info_dict['duration']))) if 'duration' in info_dict else 'unknown',
+        'thumb': info_dict['thumbnail'] if 'thumbnail' in info_dict else '',
+    }
 
-    update.message.reply_text('<strong>%s</strong>\n<i>uploader: %s\nduration: %s\n---------------------\nChoose format:</i>' % \
-        (info_dict['title'], info_dict['uploader'], info_dict['duration']), parse_mode=ParseMode.HTML)
+    # create format keyboard
+    keyboard = [
+        [InlineKeyboardButton("bestaudio", callback_data='bestaudio')],
+        [InlineKeyboardButton("mp4", callback_data='mp4'), InlineKeyboardButton("wav", callback_data='wav')],
+        [InlineKeyboardButton("abort", callback_data='abort')]
+    ]
+
+    update.message.reply_text('<strong>%-70s</strong>\n<i>by %s</i>' % (meta['title'], meta['performer']), 
+        parse_mode=ParseMode.HTML,
+        reply_markup=InlineKeyboardMarkup(keyboard))
 
     chat_data['info_dict'] = info_dict
-    return CHOOSE_FORMAT
+    return MENU_FORMAT
 
 
 
 def handle_format(bot, update, chat_data):
     """ Handle format """
-    _format = update.message.text
+    msg = update.callback_query.message
+    _format = query = update.callback_query.data
 
-    update.message.reply_text('Choose quick or advanced')
+    # remove keyboard
+    bot.edit_message_reply_markup(chat_id=msg.chat.id, message_id=msg.message_id)
+
+    if _format == 'abort':
+        msg.reply_text('<i>aborted</i>', parse_mode=ParseMode.HTML)
+        return ConversationHandler.END
+
+    if _format == 'mp4' or _format == 'wav':
+        msg.reply_text('<i>format %s not implemented yet</i>' % _format, parse_mode=ParseMode.HTML)
+        return ConversationHandler.END
+
+    # create mode keyboard
+    keyboard = [
+        [InlineKeyboardButton("advanced", callback_data='advanced'), InlineKeyboardButton("quick", callback_data='quick')]
+    ]
+
+    bot.edit_message_reply_markup(chat_id=msg.chat.id, message_id=msg.message_id, reply_markup=InlineKeyboardMarkup(keyboard))
 
     chat_data['format'] = _format
-    return QUICK_OR_ADVANCED
+    return MENU_DOWNLOAD
+
+def handle_download(bot, update, chat_data):
+    """ Handle download """
+    msg = update.callback_query.message
+    download_mode = query = update.callback_query.data
+
+    # remove keyboard
+    bot.edit_message_reply_markup(chat_id=msg.chat.id, message_id=msg.message_id)
+
+    if download_mode == 'quick':
+        return handle_quick_download(bot, msg, chat_data)
+    if download_mode == 'advanced':
+        return handle_advanced_download(bot, msg, chat_data)
 
 
-
-def handle_invalid_format(bot, update, chat_data):
-    """ Handle invalid format """
-    _format = update.message.text
-
-    update.message.reply_text('<strong>Error:</strong> <i>%s is not a valid format.</i>' % _format, parse_mode=ParseMode.HTML)
-    return CHOOSE_FORMAT
-
-
-
-def handle_quick_download(bot, update, chat_data):
-    chat_id = update.message.chat_id
+def handle_quick_download(bot, msg, chat_data):
+    chat_id = msg.chat_id
     info_dict = chat_data['info_dict']
 
+    if not 'format' in chat_data:
+        chat_data['format'] = 'bestaudio' # fallback format
+
     opts = {
-        'format': 'bestaudio/best',
-        'outtmpl': 'tmp/%(id)s'
+        'outtmpl': 'tmp/%(id)s',
+        'format': chat_data['format'],
+        'forceid': True
     }
 
     # load audio information
@@ -102,20 +140,13 @@ def handle_quick_download(bot, update, chat_data):
             filename = 'tmp/' + info_dict['id']
             log.info('Downloaded file %s' % filename)
         except youtube_dl.utils.DownloadError as e:
-            update.message.reply_text( 
+            msg.reply_text( 
                 '<strong>Error:</strong> <i>Failed to download audio from url.</i>', parse_mode=ParseMode.HTML)
             return ConversationHandler.END
-
-    metadata = {
-        'title': info_dict['title'] if 'title' in info_dict else '',
-        'performer': info_dict['creater'] if 'creater' in info_dict else info_dict['uploader'],
-        'duration': info_dict['duration'] if 'duration' in info_dict else '',
-        'thumb': info_dict['thumbnail'] if 'thumbnail' in info_dict else '',
-    }
     
 
     with open(filename, 'rb') as audio:
-        bot.send_audio(chat_id=chat_id, audio=audio, timeout=180, **metadata)
+        bot.send_audio(chat_id=chat_id, audio=audio, timeout=180, **chat_data['metadata'])
 
     # remove audio file from disc
     try:
@@ -129,7 +160,7 @@ def handle_quick_download(bot, update, chat_data):
 
 
 def handle_advanced_download(bot, update, chat_data):
-    update.message.reply_text('Advanced download not implemented yet.')
+    msg.reply_text('Advanced download not implemented yet.')
     return ConversationHandler.END
 
 
@@ -139,7 +170,7 @@ def handle_error(bot, update, error):
 
 
 
-QUICK_OR_ADVANCED, CHOOSE_FORMAT = range(2)
+MENU_FORMAT, MENU_DOWNLOAD = range(2)
 
 def initialize():
     log.info('Initialize Telegram Bot...')
@@ -153,11 +184,8 @@ def initialize():
         entry_points=[MessageHandler(Filters.all, handle_incoming_url, pass_chat_data=True)],
 
         states={
-            CHOOSE_FORMAT: [RegexHandler('(bestaudio|best)', handle_format, pass_chat_data=True), 
-                            MessageHandler(Filters.all, handle_invalid_format, pass_chat_data=True)],
-
-            QUICK_OR_ADVANCED: [RegexHandler('quick', handle_quick_download, pass_chat_data=True),
-                                RegexHandler('advanced', handle_advanced_download, pass_chat_data=True)]
+            MENU_FORMAT: [CallbackQueryHandler(handle_format, pass_chat_data=True)],
+            MENU_DOWNLOAD: [CallbackQueryHandler(handle_download, pass_chat_data=True)]
         },
 
         fallbacks=[CommandHandler('cancel', handle_cancel)],
